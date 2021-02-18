83e954e6e8be552dbff153b4edf4a28da7d95185
Valeria Copleman <valeriacopleman@gmail.com>
2021-02-17 17:01:11 -0800

    commit tree 35a2b86a1b38acd7036f92a3adeeb67b6f1a776f

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const peeps = ['Lisa', 'Kaitlin', 'Jan'];
+
+const date = "surprise"
+
+function writeCards(peeps, date) {
+    let message = []
+    for (let i = 0; i < peeps.length; i++) {
+        message.push(`Thank you, ${peeps[i]}, for the wonderful ${date} gift!`)
+    }
+    return message;
+}
+
+function countDown(num) {
+    while (num != -1) {
+        console.log(num--)
+    }
+}
